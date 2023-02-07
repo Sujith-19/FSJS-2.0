786fabf72abe489671f4d690487cab82a50becd4
Sujith-19 <shivaratris00@gmail.com>
2023-02-08 03:14:05 +0530

    Initial Commit

---
+++ JavaScript assignments/Que1.js
@@ -0,0 +1,21 @@
+// //string//
+// var firstName = "Sujith";
+// var lastName = "Shivaratri";
+// console.log(typeof 'firstName');
+
+
+//boolean//
+// let num1=10;
+// let num2=9;
+// let num=(num1>num2);
+// console.log(typeof(num));
+
+// // undefined
+// let myArr =[2,6,7,8];
+// console.log(typeof myArr[7]);
+
+
+// //null datatype
+// let naem = null;
+
+// console.log(typeof(naem));
---
+++ JavaScript assignments/Que2.js
@@ -0,0 +1,12 @@
+var list="javaScript is done"
+for(let i=0;i<list.length;i++) {
+
+}
+
+
+funtnasd(dnansoida)
+
+dnansoida()
+
+return
+
