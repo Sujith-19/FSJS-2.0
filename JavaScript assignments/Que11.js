a3833327dcbbbbcf1d66f57341ead810cff28f9f
Sujith-19 <shivaratris00@gmail.com>
2023-02-17 23:30:22 +0530

    que11 continue

---
+++ JavaScript assignments/Que11.js
@@ -0,0 +1,12 @@
+// let date=new Date().getFullYear();
+// console.log(date);
+
+// let date2=new Date().getMonth();
+// console.log(date2);
+
+// let date3=new Date().getDate();
+// console.log(date3);
+
+// let date4=new Date().getDay();
+// console.log(date4);
+
