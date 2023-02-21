627af25922311be0c926707b99943651c0760c82
Sujith-19 <shivaratris00@gmail.com>
2023-02-21 18:20:18 +0530

    Que19 update

---
+++ JavaScript assignments/Que16.js
@@ -0,0 +1,11 @@
+const takeInput= prompt('Enter the month: ');
+let season = {
+    summer : ['july','june','august'],
+    winter: ["february", "december", "january"],
+    spring: ['March','april','may'],
+    autumn: ['september','october','november']
+
+
+};
+takeInput.toLowerCase();
+
