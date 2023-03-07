d816bc2c4a6580325e5254bd25963b8a05c57cad
Sujith-19 <shivaratris00@gmail.com>
2023-03-07 15:00:18 +0530

    DOM Completed

--- DOM Assignment/DOM Assignment 2.0 1,2,3/about/about.js
+++ DOM Assignment/DOM Assignment 2.0 1,2,3/about/about.js
@@ -9,3 +9,20 @@ accordian.forEach((element) => {
     }
   });
 });
+
+let accordian1 = document.querySelectorAll(".accordian h3");
+accordian1.forEach((element) => {
+  element.style.background = "#d3deff"
+})
+
+let skill = document.querySelector(".accordian:last-child")
+let div = document.createElement("div")
+div.setAttribute("class", "accordian")
+let h3 = document.createElement("h3")
+h3.style.background = "#d3deff"
+h3.append("Skills")
+let p = document.createElement("p")
+p.append("I posses a very good command over the Full stack webdevelopment technologies like Mern which can be seen in my work over the Github")
+div.append(h3,p)
+p.style.display = "block";
+skill.parentNode.insertBefore(div, skill.nextSibling)
---
+++ DOM Assignment/DOM Assignment 2.0 1,2,3/index.js
@@ -0,0 +1,41 @@
+// let element1 = document.querySelector("header nav ul li:last-child")
+// element1.innerHTML = "<a>Projects</a>"
+
+// let element2  = document.createElement("li")
+// element2.innerHTML = "<a>Hire Me</a>"
+
+// let element3 = document.querySelector("header nav ul")
+// element3.appendChild(element2)
+
+// let footer = document.querySelector("footer ul")
+// footer.remove()
+
+// let element1 = document.querySelector("header nav ul li:last-child")
+// element1.innerHTML = "<a>Projects</a>"
+
+// let element2 = document.querySelector(".search-field input")
+// element2.placeholder = "Search My Project"
+
+// let footer = document.querySelector("footer ul")
+// footer.innerHTML = ""
+
+// let element1 = document.querySelector("header nav ul li:last-child")
+// element1.innerHTML = "<a>Projects</a>"
+
+// let element2 = document.querySelectorAll(".hero-left-section p span")
+// element2[1].textContent = "an Employee"
+// element2[2].textContent = "iNeuron Intelligence Pvt Ltd"
+
+// let element1 = document.querySelector("header nav ul li:last-child")
+// element1.innerHTML = "<a>Projects</a>"
+
+// let element2 = document.querySelector(".hero-right-section img")
+// element2.src = "./hitesh.jpg"
+
+// let element1 = document.querySelector("header nav ul li:last-child")
+// element1.innerHTML = "<a>Projects</a>"
+
+// let element2 = document.querySelector(".hero-right-section-btns")
+// let button = document.createElement('button')
+// button.innerHTML = "Support Me"
+// element2.append(button)
